61417e19c7571e9d661e2b026a412291c3f3a755
Ohno Shoya <shoya.inf@gmail.com>
2020-04-12 10:06:59 +0000

    a

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.profile')
-@section('title', 'プロフィールの削除')
+@section('title', 'プロフィールの編集')
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィールの削除</h2>
+                <h2>プロフィールの編集</h2>
             </div>
         </div>
     </div>
