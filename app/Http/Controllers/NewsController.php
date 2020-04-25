e05be9b4fb8426fcf0ace4bae93fba2f977e4b4e
Ohno Shoya <shoya.inf@gmail.com>
2020-04-25 04:48:21 +0000

    課題

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,24 +1,75 @@
 <?php
-
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\HTML;
+use App\Http\Controllers\Controller;
 
 use App\News;
 
 class NewsController extends Controller
 {
-    public function index(Request $request)
-    {
-        $posts = News::all()->sortByDesc('updated_at');
+  public function add()
+  {
+      return view('admin.news.create');
+  }
+  public function create(Request $request)
+  {
+      
+      $this->validate($request, News::$rules);
+      $news = new News;
+      $form = $request->all();
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      unset($form['_token']);
+      unset($form['image']);
+      $news->fill($form);
+      $news->save();
+      return redirect('admin/news/create');
+  }
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  public function edit(Request $request)
+  {
+      $news = News::find($request->id);
+      if (empty($news)) {
+        abort(404);    
+      }
+      return view('admin.news.edit', ['news_form' => $news]);
+  }
+  public function update(Request $request)
+  {
+      $this->validate($request, News::$rules);
+      $news = News::find($request->id);
+      $news_form = $request->all();
+      if (isset($news_form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+        unset($news_form['image']);
+      } elseif (isset($request->remove)) {
+        $news->image_path = null;
+        unset($news_form['remove']);
+      }
+      unset($news_form['_token']);
+      $news->fill($news_form)->save();
 
-        if (count($posts) > 0) {
-            $headline = $posts->shift();
-        } else {
-            $headline = null;
-        }
+      return redirect('admin/news');
+  }
+  public function delete(Request $request)
+  {
+      $news = News::find($request->id);
+      $news->delete();
+      return redirect('admin/news/');
+  }  
 
-        return view('news.index', ['headline' => $headline, 'posts' => $posts]);
-    }
 }
